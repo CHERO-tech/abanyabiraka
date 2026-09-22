4c4b3b08cfddc0c392e3807671ceed995898a5ff
CherlyIC <cherlyimfurankunda@gmail.com>
2026-09-22 14:27:07 +0200

    feat(frontend):add booking request page

--- frontend/.next/types/app/workers/page.ts
+++ frontend/.next/types/app/workers/page.ts
@@ -1,8 +1,8 @@
-// File: D:\abanyabiraka\frontend\app\workers\[id]\page.js
-import * as entry from '../../../../../app/workers/[id]/page.js'
+// File: D:\abanyabiraka\frontend\app\workers\page.js
+import * as entry from '../../../../app/workers/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../app/workers/[id]/page.js')
+type TEntry = typeof import('../../../../app/workers/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
