234a06deda4dd88d78ce572a329908103d8e5cfd
CherlyIC <cherlyimfurankunda@gmail.com>
2026-09-25 09:41:30 +0200

    feat:add worker profile

--- frontend/.next/types/app/dashboard/worker/profile/page.ts
+++ frontend/.next/types/app/dashboard/worker/profile/page.ts
@@ -1,8 +1,8 @@
-// File: D:\abanyabiraka\frontend\app\dashboard\worker\page.js
-import * as entry from '../../../../../app/dashboard/worker/page.js'
+// File: D:\abanyabiraka\frontend\app\dashboard\worker\profile\page.js
+import * as entry from '../../../../../../app/dashboard/worker/profile/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../app/dashboard/worker/page.js')
+type TEntry = typeof import('../../../../../../app/dashboard/worker/profile/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
